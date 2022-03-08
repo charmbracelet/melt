95d8d81287ca54d3df56d31054742f899cb117d2
Carlos A Becker <caarlos0@gmail.com>
2022-03-08 14:30:52 -0300

    test: moar tests
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- melt.go
+++ melt.go
@@ -1,3 +1,6 @@
+// Package melt provides function to create a mnemonic set of keys from a
+// ed25519 private key, and restore that key from the same mnemonic set of
+// words.
 package melt
 
 import (
@@ -7,15 +10,18 @@ import (
 	"github.com/tyler-smith/go-bip39"
 )
 
+// ToMnemonic takes a ed25519 private key and returns the list of words.
 func ToMnemonic(key *ed25519.PrivateKey) (string, error) {
 	words, err := bip39.NewMnemonic(key.Seed())
 	if err != nil {
-		return "", fmt.Errorf("could not create a mnemonics: %w", err)
+		return "", fmt.Errorf("could not create a mnemonic set of words: %w", err)
 	}
 
 	return words, nil
 }
 
+// FromMnemonic takes a mnemonic list of words and returns an ed25519
+// private key.
 func FromMnemonic(mnemonic string) (ed25519.PrivateKey, error) {
 	seed, err := bip39.EntropyFromMnemonic(mnemonic)
 	if err != nil {
--- melt_test.go
+++ melt_test.go
@@ -1,6 +1,49 @@
 package melt
 
-import "testing"
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"testing"
+
+	"github.com/matryer/is"
+)
 
 func TestToMnemonic(t *testing.T) {
+	t.Run("invalid", func(t *testing.T) {
+		t.Skipf("this panics inside ed25519... not sure how to properly test an invalid key")
+		is := is.New(t)
+		key := ed25519.PrivateKey([]byte{})
+		w, err := ToMnemonic(&key)
+		is.Equal(w, "")
+		is.True(err != nil)
+	})
+
+	t.Run("valid", func(t *testing.T) {
+		is := is.New(t)
+		_, k, err := ed25519.GenerateKey(rand.Reader)
+		is.NoErr(err)
+		w, err := ToMnemonic(&k)
+		is.NoErr(err)
+		is.True(w != "")
+	})
+}
+
+func TestFromMnemonic(t *testing.T) {
+	t.Run("invalid", func(t *testing.T) {
+		is := is.New(t)
+		key, err := FromMnemonic("nope nope nope")
+		is.Equal(key, nil)
+		is.True(err != nil)
+	})
+
+	t.Run("valid", func(t *testing.T) {
+		is := is.New(t)
+		key, err := FromMnemonic(`
+			alter gap broom kitten orient over settle work honey rule
+			coach system wage effort mask void solid devote divert
+			quarter quote broccoli jaguar lady
+		`)
+		is.NoErr(err)
+		is.True(key != nil)
+	})
 }
