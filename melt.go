a1f382161ccc3247b7824830cd06bf38ba6071a9
Carlos A Becker <caarlos0@gmail.com>
2022-03-16 15:48:55 -0300

    undo
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

---
+++ melt.go
@@ -0,0 +1,35 @@
+// Package melt provides function to create a mnemonic set of keys from a
+// ed25519 private key, and restore that key from the same mnemonic set of
+// words.
+package melt
+
+import (
+	"crypto/ed25519"
+	"fmt"
+
+	"github.com/tyler-smith/go-bip39"
+)
+
+// ToMnemonic takes a ed25519 private key and returns the list of words.
+func ToMnemonic(key *ed25519.PrivateKey) (string, error) {
+	return toMnemonic(key.Seed())
+}
+
+func toMnemonic(seed []byte) (string, error) {
+	words, err := bip39.NewMnemonic(seed)
+	if err != nil {
+		return "", fmt.Errorf("could not create a mnemonic set of words: %w", err)
+	}
+
+	return words, nil
+}
+
+// FromMnemonic takes a mnemonic list of words and returns an ed25519
+// private key.
+func FromMnemonic(mnemonic string) (ed25519.PrivateKey, error) {
+	seed, err := bip39.EntropyFromMnemonic(mnemonic)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get seed from mnemonic: %w", err)
+	}
+	return ed25519.NewKeyFromSeed(seed), nil
+}
